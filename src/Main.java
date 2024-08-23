40eb478f07952845ee2bb0fef341c79a38a2e6f4
Murilo751 <murilo.matos2005@gmail.com>
2024-08-23 16:10:03 -0300

    fix formatting
    Signed-off-by: Murilo751 <murilo.matos2005@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         connection db = new connection();
